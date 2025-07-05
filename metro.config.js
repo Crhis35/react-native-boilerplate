62d11029379fe6770879d2eb7d3b452d83105bf0
Crhistian Caraballo <cristianjcj35@gmail.com>
2025-07-05 13:49:31 -0500

    feat: updated rn version

--- metro.config.js
+++ metro.config.js
@@ -7,13 +7,14 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
   transformer: {
     babelTransformerPath: require.resolve(
       'react-native-svg-transformer/react-native',
     ),
+    unstable_allowRequireContext: true,
   },
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
